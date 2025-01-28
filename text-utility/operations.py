3a10316f5cb460fa4af6b509e0eb050e9e77bb3e
Naili Mahdi <39757299+ftnt-cse@users.noreply.github.com>
2025-01-28 14:07:50 +0400

    1.1.0
    - extract macros - added
    - sentence similarity - rewritten
    - missing: tesseract rpm dependency (connector RPM should install tesseract.x86_64)

--- text-utility/operations.py
+++ text-utility/operations.py
@@ -5,57 +5,36 @@ Copyright (c) 2024 Fortinet Inc
 Copyright end
 """
 
-import logging
 from .constants import *
 from connectors.core.connector import get_logger, ConnectorError
-from sentence_transformers import SentenceTransformer, util
 from connectors.cyops_utilities.builtins import download_file_from_cyops
 from PIL import Image
+from rapidfuzz import fuzz
+from oletools.olevba import VBA_Parser
 import pytesseract
 import os
+import logging
 
 
 logger = get_logger('text-utility')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 
 def sentence_similarity(config, params):
-    scores = {}
-    report = {}
-    sentences = [ params.get("sentence") ]
-    
-    sentences_to_compare_with = params.get("sentences_to_compare_with").replace(" ","").split(",") if "," in params.get("sentences_to_compare_with") else params.get("sentences_to_compare_with")
 
-    if not isinstance(sentences_to_compare_with,list) and len(sentences_to_compare_with) < 2:
+    scores = {}
+    sentence = params.get("sentence")
+    sentences_to_compare_with = params.get("sentences_to_compare_with")
+    if not isinstance(sentences_to_compare_with,list):
         ConnectorError(A1PARAM2_ERROR)
         raise Exception(A1PARAM2_ERROR)
-    
-    sentences.extend(sentences_to_compare_with)
+    for s in sentences_to_compare_with:
+        # Similarity in %
+        similarity = fuzz.ratio(sentence, s)
+        scores.update({s: similarity})
+    return scores
+        
 
-    model = SentenceTransformer('distilbert-base-nli-mean-tokens')
-
-    sentence_embeddings = model.encode(sentences)
-
-    for sentence, embedding in zip(sentences[1:], sentence_embeddings[1:]):
-        score = util.pytorch_cos_sim(sentence_embeddings[0], embedding)
-        logger.debug(f"[{sentences[0]}] compared to [{sentence}], scores [{score.item()}]")
-        scores.update({sentence:score.item()})
-        report.update({sentence:score.item()})
-        
-    sorted_scores = sorted(scores, key=scores.get, reverse=True)
-    if len(sorted_scores) > 0:
-        logger.info(f"[{sentences[0]}] and [{sorted_scores[0]}] are similar")
-        return {
-            "sentence":sentences[0],
-            "similarTo":sorted_scores[0],
-            "scores":report
-        }
-    else:
-        ConnectorError(GENERIC_ERROR)
-        raise Exception(GENERIC_ERROR)
-        
-        
-        
 def image_to_text(config, params):
     
     image_iri = params.get("image_iri")
@@ -70,3 +49,27 @@ def image_to_text(config, params):
     os.remove(file_path)
     return ocr_text
   
+
+def extract_macros(config, params):
+
+    macros = []
+    file_iri = params.get("file_iri")
+    res = download_file_from_cyops(file_iri)
+    file_path = "{0}/{1}".format('/tmp', res['cyops_file_path'])
+    vba_parser = VBA_Parser(file_path)
+    logger.info(f"File temporarily saved at: {file_path}")     
+
+    if vba_parser.detect_vba_macros():
+        # Loop through all the macros
+        for (filename, stream_path, vba_filename, vba_code) in vba_parser.extract_macros():
+            macros.append({
+            "filename": filename,
+            "stream_path": stream_path,
+            "file_name": vba_filename,
+            "macro":vba_code
+            })
+    else:
+        macros.append({"result":"No macros found."})
+    vba_parser.close()
+    os.remove(file_path)
+    return macros  
